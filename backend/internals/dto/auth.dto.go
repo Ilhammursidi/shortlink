bf8ca6bd77e6e91b296aeeb1d32f502e01fc4942
ilhammursidi <imursidi37@gmail.com>
2026-06-17 17:46:16 +0700

    feat: add auth dto

---
+++ backend/internals/dto/auth.dto.go
@@ -0,0 +1,19 @@
+package dto
+
+type RegisterRequest struct {
+	Name            string `json:"name" binding:"required"`
+	Email           string `json:"email" binding:"required"`
+	Password        string `json:"password" binding:"required"`
+	ConfirmPassword string `json:"confirm_password" binding:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
