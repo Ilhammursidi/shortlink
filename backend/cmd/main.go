895c6282fa2c78962f27bc1f1f535b35f0dfd2ce
ilhammursidi <imursidi37@gmail.com>
2026-06-17 22:53:45 +0700

    feat: make entry point

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/ilhammursidi/shortlink/internals/di"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .env file found, using environment variables")
+	}
+
+	r := di.Init()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8888"
+	}
+	log.Printf("server running on port %s", port)
+	if err := r.Run(":" + port); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
