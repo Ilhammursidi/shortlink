43ea8ebc14aa8c50bc507f1753cfdf8bbcb2ed2f
ilhammursidi <imursidi37@gmail.com>
2026-06-18 11:14:15 +0700

    feat: add upload picture controller

--- backend/internals/controller/user.controller.go
+++ backend/internals/controller/user.controller.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ilhammursidi/shortlink/internals/dto"
 	"github.com/ilhammursidi/shortlink/internals/services"
 )
 
@@ -19,20 +20,23 @@ func NewUserController(userService *services.UserService) *UserController {
 func (u *UserController) UploadPicture(c *gin.Context) {
 	userID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse("invalid user id"))
 		return
 	}
 
 	file, err := c.FormFile("picture")
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, dto.ErrorResponse("picture is required"))
 		return
 	}
 
 	picturePath, err := u.userService.UploadPicture(userID, file)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, dto.ErrorResponse(err.Error()))
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"picture": picturePath})
+
+	c.JSON(http.StatusOK, dto.SuccessResponse("Picture uploaded successfully", gin.H{
+		"picture": picturePath,
+	}))
 }
