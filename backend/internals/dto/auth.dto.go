b9171dc1d93262fa3037fa731220e58e5ebe522a
ilhammursidi <imursidi37@gmail.com>
2026-06-19 14:52:59 +0700

    refactor:add tag validator min for Password and tag  email for Email

--- backend/internals/dto/auth.dto.go
+++ backend/internals/dto/auth.dto.go
@@ -2,8 +2,8 @@ package dto
 
 type RegisterRequest struct {
 	Name            string `json:"name" binding:"required"`
-	Email           string `json:"email" binding:"required"`
-	Password        string `json:"password" binding:"required"`
+	Email           string `json:"email" binding:"required,email"`
+	Password        string `json:"password" binding:"required,min=8"`
 	ConfirmPassword string `json:"confirm_password" binding:"required"`
 }
 
